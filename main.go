6e3459bf13ec30fd9ad0b6b36d402753d0e77f26
hnyguo@hotmail.com <hnyguo@hotmail.com>
2024-03-19 14:08:43 +0800

    print values after seq

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 const DIM = 20
 const DNASIZE = 7
-const POPSIZE = 20
-const MAXIT = 100
+const POPSIZE = 30
+const MAXIT = 500
 const LB = 0
 const UB = 3
-const DNASETITERATION = 1000
+const DNASETITERATION = 200
 
 func main() {
 	fitChan := CreateWorker(100, 100, 10)
@@ -27,12 +27,37 @@ func main() {
 			}
 		}
 		fmt.Println("Done")
-		for _, inv := range dnaSet {
+		for ind, inv := range dnaSet {
 			DNAString, err := inv.seq.ToStr()
 			if err != nil {
 				panic("error while decoding")
 			}
-			fmt.Println(DNAString)
+			fmt.Print(DNAString, " ")
+			// continuity
+			fitChan.ctIn <- seqMapSingle{ind, inv.seq}
+			fmt.Print((<-fitChan.ctRe).value, " ")
+			// hairpin
+			fitChan.hpIn <- seqMapSingle{ind, inv.seq}
+			fmt.Print((<-fitChan.hpRe).value, " ")
+			// hm
+			hmList := make([]float64, len(dnaSet))
+			for j, o := range dnaSet {
+				fitChan.hmIn <- seqMapPair{ind, j, inv.seq, o.seq}
+				hmList[j] = (<-fitChan.hmRe).value
+			}
+			fmt.Print(sum(hmList), " ")
+			// sm
+			smList := make([]float64, len(dnaSet))
+			for j, o := range dnaSet {
+				if j != ind {
+					fitChan.smIn <- seqMapPair{ind, j, inv.seq, o.seq}
+					smList[j] = (<-fitChan.smRe).value
+				}
+			}
+			fmt.Print(sum(smList), " ")
+			//mt
+			fitChan.mtIn <- seqMapSingle{ind, inv.seq}
+			fmt.Printf("%.4f\n", (<-fitChan.mtRe).value)
 		}
 	}
 
@@ -42,4 +67,11 @@ func RandomDNASet(dim, size uint) []individual {
 	pop := CreatePopulation(dim, size, 0, 0, 3)
 	//pop.Fitness(fitFunc)
 	return pop.individuals
+}
+func sum(l []float64) float64 {
+	var s float64
+	for i := range l {
+		s += l[i]
+	}
+	return s
 }
