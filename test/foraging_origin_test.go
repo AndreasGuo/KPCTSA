e57f0d7512e4fdd2ffba4226bbd8ce989dd62fef
hnyguo@hotmail.com <hnyguo@hotmail.com>
2024-12-05 20:28:05 +0800

    detailed test on adjustment: foraging

---
+++ test/foraging_origin_test.go
@@ -0,0 +1,127 @@
+package test
+
+import (
+	"GoDNA/algorithm"
+	DNAType "GoDNA/algorithm/dnatype"
+	"log"
+	"math"
+	"math/rand"
+	"testing"
+)
+
+// PO + NDSort + Knee Point
+func (po *PO) IterationForaging(hyperPlaneNorm bool, origin bool, cd bool) *DNAType.DNAAgent {
+	logger := log.Default()
+	islog := false
+	fits := po.Pop.Fit()
+	ZMin := po.Pop.ZMin()
+	selectedIndex, _ := algorithm.NDKPSort(fits, ZMin, po.Pop.Size(), hyperPlaneNorm, cd)
+
+	gbest := po.Pop.At(selectedIndex).Variance()
+
+	for it := 0; it < int(po.MaxIteration); it++ {
+		oldPop := po.Pop.Clone()
+		popMean := Mean(po.Pop)
+		for i := 0; i < int(po.Pop.Size()); i++ {
+			if i == selectedIndex {
+				continue
+			}
+			st := rand.Intn(4)
+			x := po.Pop.At(i).Variance()
+			if islog {
+				logger.Println("it: ", it, "st: ", st)
+				logger.Println("origin variance: ", x)
+			}
+
+			if origin {
+				ost0(x, gbest, popMean, po.Pop.VarianceDim(), it, po.MaxIteration)
+			} else {
+				st0(x, gbest, popMean, po.Pop.VarianceDim(), it, po.MaxIteration)
+			}
+
+			out := make([]int, len(x))
+			for k := range x {
+				out[k] = int(math.Round(x[k]))
+			}
+			if islog {
+				logger.Println("After st: ", out)
+			}
+			//boundary control
+			for j := 0; j < po.Pop.VarianceDim(); j++ {
+				x[j] = max(x[j], po.Pop.LB())
+				x[j] = min(x[j], po.Pop.UB())
+			}
+			for k := range x {
+				out[k] = int(math.Round(x[k]))
+			}
+			if islog {
+				logger.Println("After boundary: ", out)
+			}
+			po.Pop.UpdatePosition(i, x)
+		}
+		po.Pop.PostWork()
+
+		po.Pop.Join(oldPop)
+
+		fits = po.Pop.Fit()
+		ZMin = po.Pop.ZMin()
+		bestIdx, selectedIndex := algorithm.NDKPSort(fits, ZMin, po.Pop.Size()/2, hyperPlaneNorm, cd)
+		gbest = po.Pop.At(bestIdx).Variance()
+		po.Pop.Select(selectedIndex)
+		if it%50 == 0 || it == po.MaxIteration-1 {
+			writeCSV(fits, "foraging", it, origin)
+		}
+	}
+
+	return po.Pop.At(selectedIndex)
+}
+
+func ost0(x, gbest, popMean []float64, dim, it, maxIt int) {
+	levyDim := Levy(dim)
+	for i := 0; i < dim; i++ {
+		r := rand.Float64()
+		x[i] = (x[i] - gbest[i]) * levyDim[i] * r
+		x[i] += (1 - r) * popMean[i] * math.Pow(1-float64(it)/float64(maxIt), 2*float64(it)/float64(maxIt))
+	}
+}
+
+func st0(x, gbest, popMean []float64, dim, it, maxIt int) {
+	levyDim := Levy(dim)
+	p1 := float64(4)
+	for i := 0; i < dim; i++ {
+		r := rand.Float64()
+		x[i] += (x[i] - gbest[i]) * levyDim[i] * r / p1
+		x[i] += (1 - r) * popMean[i] * math.Pow(1-float64(it)/float64(maxIt), 2*float64(it)/float64(maxIt)) / p1
+	}
+}
+
+func TestPOSt0(T *testing.T) {
+	fitChan := DNAType.CreateWorker(100, 100, 10)
+	defer fitChan.Close()
+
+	// init dna set
+	var dnaSet = baseDNAs()
+	var zmin = make([]float64, 5)
+	for j := range zmin {
+		zmin[j] = 400
+	}
+
+	for i := range dnaSet {
+		fitFunc := DNAType.FitnessCall(dnaSet, i, fitChan, 2e-2, true)
+		singleInvSlice := []*DNAType.DNAAgent{dnaSet[i]}
+		fits, _ := fitFunc(singleInvSlice)
+		dnaSet[i].SetObjs(fits[0])
+	}
+
+	index := 2
+	//fmt.Println("To Optimize: ", index)
+	fitFunc := DNAType.FitnessCall(dnaSet, index, fitChan, 2e-2, true)
+	alg := PO{Pop: nil, MaxIteration: 200}
+	pop := new(DNAType.DNAPopulation)
+	pop.SetConfig(200, 20, 4, 0, 3)
+	pop.SetFitFunc(fitFunc)
+	alg.Initialize(pop, dnaSet[index])
+	inv := alg.IterationForaging(false, true, false)
+	PrintDNASet(dnaSet, fitChan)
+	dnaSet[index] = inv
+}
---
+++ test/utility.go
@@ -0,0 +1,169 @@
+package test
+
+import (
+	DNAType "GoDNA/algorithm/dnatype"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+type PO struct {
+	Pop          *DNAType.DNAPopulation
+	MaxIteration int
+}
+
+func (po *PO) Initialize(pop *DNAType.DNAPopulation, inds ...*DNAType.DNAAgent) {
+	pop.Init()
+	if len(inds) > 0 {
+		pop.Append(inds)
+	}
+	po.Pop = pop
+}
+
+func Levy(lh int) []float64 {
+	beta := 1.5
+	sigma := math.Gamma(1+beta) * math.Sin(math.Pi*beta/2)
+	sigma /= math.Gamma(1+beta/2) * beta * math.Pow(1, (beta-1)/2)
+	sigma = math.Pow(sigma, 1/beta)
+	u := make([]float64, lh)
+	for i := range u {
+		u[i] = sigma * rand.NormFloat64() / math.Pow(math.Abs(rand.NormFloat64()), 1/beta)
+	}
+	return u
+}
+
+func Mean(pop *DNAType.DNAPopulation) []float64 {
+	size := pop.Size()
+	dim := pop.VarianceDim()
+	out := make([]float64, dim)
+	for i := range size {
+		v := pop.At(i).Variance()
+		for j := range dim {
+			out[j] += v[j]
+		}
+	}
+
+	for j := range dim {
+		out[j] /= float64(size)
+	}
+
+	return out
+}
+
+func PrintDNASet(dnaSet []*DNAType.DNAAgent, fitChan *DNAType.FitChan) string {
+	var result = ""
+	for ind, inv := range dnaSet {
+		DNAString, err := inv.String()
+		if err != nil {
+			panic("error while decoding")
+		}
+		result += strconv.Itoa(ind) + " " + DNAString
+
+		// continuity
+		fitChan.CtIn <- DNAType.SeqMapSingle{Index: ind, Seq: inv.Represent()}
+		ct := (<-fitChan.CtRe).Value
+		result += " " + strconv.FormatFloat(ct, 'f', 4, 64)
+
+		// hairpin
+		fitChan.HpIn <- DNAType.SeqMapSingle{Index: ind, Seq: inv.Represent()}
+		hp := (<-fitChan.HpRe).Value
+		result += " " + strconv.FormatFloat(hp, 'f', 4, 64)
+
+		// hm
+		hmList := make([]float64, len(dnaSet))
+		for j, o := range dnaSet {
+			fitChan.HmIn <- DNAType.SeqMapPair{Index1: ind, Index2: j, Seq1: inv.Represent(), Seq2: o.Represent()}
+			hmList[j] = (<-fitChan.HmRe).Value
+		}
+		hm := sum(hmList)
+		result += " " + strconv.FormatFloat(hm, 'f', 4, 64)
+
+		// sm
+		smList := make([]float64, len(dnaSet))
+		for j, o := range dnaSet {
+			if j != ind {
+				fitChan.SmIn <- DNAType.SeqMapPair{Index1: ind, Index2: j, Seq1: inv.Represent(), Seq2: o.Represent()}
+				smList[j] = (<-fitChan.SmRe).Value
+			}
+		}
+		sm := sum(smList)
+		result += " " + strconv.FormatFloat(sm, 'f', 4, 64)
+
+		//mt
+		fitChan.MtIn <- DNAType.SeqMapSingle{Index: ind, Seq: inv.Represent()}
+		mt := (<-fitChan.MtRe).Value
+		result += " " + strconv.FormatFloat(mt, 'f', 4, 64) + "\n"
+	}
+	fmt.Println(result)
+	return result
+}
+
+func sum[T int | float64](lt []T) T {
+	var s T = 0
+	for i := range lt {
+		s += lt[i]
+	}
+	return s
+}
+
+func writeCSV(data [][]float64, behavior string, it int, origin bool) {
+	isOrigin := "origin"
+	if !origin {
+		isOrigin = "adjust"
+	}
+	filename := fmt.Sprintf("obj-%s-%d-%s.csv", behavior, it+1, isOrigin)
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	for _, value := range data {
+		v := []string{}
+		for _, iv := range value {
+			v = append(v, strconv.FormatFloat(float64(iv), 'f', 2, 64))
+		}
+		writer.Write(v)
+	}
+}
+
+func randomDNASet(dim, size int) []*DNAType.DNAAgent {
+	dnaSet := []*DNAType.DNAAgent{}
+	for i := 0; i < size; i++ {
+		dnaSet = append(dnaSet, DNAType.CreateDNAAgent(dim, 0, 3))
+	}
+	return dnaSet
+}
+
+func agentFromStr(str string) *DNAType.DNAAgent {
+	seq, _ := DNAType.ToSeq(str)
+	agent := DNAType.DNAAgent{}
+	// init raw variance
+	agent.DNARowVariance = make([]float64, len(str))
+	for i := 0; i < len(str); i++ {
+		agent.DNARowVariance[i] = float64(seq[i])
+	}
+	agent.RepairAndToSeq()
+	return &agent
+}
+
+func baseDNAs() []*DNAType.DNAAgent {
+	dnas := []*DNAType.DNAAgent{}
+	// CCTCCACTATCAGAACTACC
+	// AACCTAACCTCCTCTCCATC
+	agent1 := agentFromStr("CCTCCACTATCAGAACTACC")
+	dnas = append(dnas, agent1)
+
+	agent2 := agentFromStr("AACCTAACCTCCTCTCCATC")
+	dnas = append(dnas, agent2)
+
+	agent3 := DNAType.CreateDNAAgent(20, 0, 3)
+	dnas = append(dnas, agent3)
+
+	return dnas
+}
